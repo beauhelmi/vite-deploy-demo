f03327d4b5f3ece7575864863aa0a7166d2bd318
beauhelmi <beauhelmi@gmail.com>
2022-11-11 18:30:12 +0800

    import path svg failed, trying again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import public from './public/vite.svg'
+import public from '../public/vite.svg'
 import './App.css'
 
 function App() {
